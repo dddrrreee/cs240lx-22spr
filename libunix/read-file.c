2e78a2f99c9ad84c15c7585ebb0e5a2fde4bddf7
engler <ddd.rrr.eee@gmail.com>
2022-03-31 18:32:58 -0700

    minor

--- libunix/read-file.c
+++ libunix/read-file.c
@@ -5,25 +5,45 @@
 
 #include "libunix.h"
 
-// read in file <name>
-// returns:
-//  - pointer to the code.  pad code with 0s up to the
-//    next multiple of 4.  
-//  - bytes of code in <size>
+// read entire file into buffer.  return it.   zero pads to a 
+// multiple of 4.
 //
-// fatal error open/read of <name> fails.
-// 
-// How: 
-//    - use stat to get the size of the file.
-//    - round up to a multiple of 4.
-//    - allocate a buffer  --- 
-//    - zero pads to a // multiple of 4.
-//    - read entire file into buffer.  
-//    - make sure any padding bytes have zeros.
-//    - return it.   
-//
-// make sure to close the file descriptor (this will
-// matter for later labs).
+// make sure to cleanup!
 void *read_file(unsigned *size, const char *name) {
-    unimplemented();
+    int fd = open(name, O_RDONLY);
+    if(fd < 0)
+        sys_die(open, "can't open pi program file: <%s>\n", name);
+
+    // get the size (and other attributes)
+    struct stat s;
+    if(fstat(fd, &s) < 0)
+        sys_die(fstat, "can't stat file <%s>\n", name);
+
+    // allocate buffer and read the file contents into it.   allocate
+    // worst case extra bytes.
+    uint8_t *buf = calloc(1,s.st_size+4);
+    assert(buf);
+
+    int n = read(fd, buf, s.st_size);
+    if(n != s.st_size)
+        panic ("bad read file <%s>: got %d bytes expected %lu\n",
+                        name, n, (unsigned long) s.st_size);
+    close(fd);
+
+    *size = n;
+    return buf;
+}
+
+int read_file_noalloc(const char *name, void *buf, unsigned maxsize) {
+    int fd = open(name, O_RDONLY);
+    if(fd < 0)
+        sys_die(open, "can't open pi program file: <%s>\n", name);
+
+    int res = read(fd, buf, maxsize);
+    if(res < 0)
+        sys_die(read, read failed);
+    if(res == maxsize)
+        panic("buffer too small\n");
+    close(fd);
+    return res;
 }
